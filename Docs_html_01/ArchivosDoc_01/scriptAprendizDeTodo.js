c3cb5ae111cfc4ffa144fdd82deda5397d8f69c7
Mauricio723 <salvadorvilches1972@gmail.com>
2023-09-21 01:00:06 -0300

    control de cambio de colores

---
+++ Docs_html_01/ArchivosDoc_01/scriptAprendizDeTodo.js
@@ -0,0 +1,63 @@
+/* Script AprendizDeTodo */
+
+  let colorFondoNuevo;  
+  let colorTextoNuevo;
+	
+  let colorOscuro;
+  let colorClaro;   
+  
+  let rootVariableStyle;
+  
+  let valorInputRadio;
+  
+function inicioApp() {
+		    	
+	rootVariableStyle = document.documentElement;
+	
+	colorOscuro = "black";
+	colorClaro = "white";
+	
+	obtenerValorInputRadio();	 	
+		
+}
+ 
+    function obtenerValorInputRadio() {
+	    	
+		valorInputRadio = document.querySelector('input[name="colores_pagina"]:checked').value
+					
+		establecerColoresPagina();	
+		
+    }      		
+		
+	function establecerColoresPagina() {
+		
+		if (valorInputRadio === "fondo_claro") {
+			
+			colorFondoNuevo = colorClaro;
+			colorTextoNuevo = colorOscuro;
+						
+		} 
+		if (valorInputRadio === "fondo_oscuro") {
+			
+			colorFondoNuevo = colorOscuro;
+			colorTextoNuevo = colorClaro;
+						
+		}
+				
+		rootVariableStyle.style.setProperty("--color_fondo_01", colorFondoNuevo);
+		rootVariableStyle.style.setProperty("--color_texto_01", colorTextoNuevo);
+		
+	}	
+	
+	function changeSelectOscuro() {		
+		colorOscuro = document.getElementById("colorOscuroSelect").value;		
+		establecerColoresPagina();		
+	}
+	
+	function changeSelectClaro() {
+		colorClaro = document.getElementById("colorClaroSelect").value;
+		establecerColoresPagina();
+	}
+	 	
+	window.addEventListener("load", inicioApp, false);
+	
